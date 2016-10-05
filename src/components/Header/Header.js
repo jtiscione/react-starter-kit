6e15d55abb8c30607852092c2f75011e1d88b669
tiscione <tiscione@gmail.com>
2016-10-04 22:10:53 -0600

    Fixed release build problem from react-bootstrap

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,3 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Header.css';
@@ -22,10 +13,10 @@ import {
   Navbar,
   Nav,
   NavItem,
-  NavDropdown,
-  MenuItem,
 } from 'react-bootstrap';
 
+const Hdr = Navbar.Header;
+const Brand = Navbar.Brand;
 
 function Header() {
   return (
@@ -46,13 +37,13 @@ function Header() {
     Replaced the above with:
     */
     <Navbar>
-      <Navbar.Header>
-        <Navbar.Brand>
+      <Hdr>
+        <Brand>
           <Link className={s.brand} to="/">Redux Chess</Link>
-        </Navbar.Brand>
-      </Navbar.Header>
+        </Brand>
+      </Hdr>
       <Nav bsStyle="tabs">
-        <NavItem eventKey={1} href="/play">Play Chess</NavItem>
+        <NavItem eventKey={1} href="/play">Play</NavItem>
         <NavItem eventKey={2} href="/about">About</NavItem>
         <NavItem eventKey={3} href="/contact">Contact</NavItem>
         <NavItem eventKey={4} href="/login">Log In</NavItem>
