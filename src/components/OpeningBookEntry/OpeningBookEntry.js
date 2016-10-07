9ddab556c78ceb514bb029a65ec645d51dbecaf6
tiscione <tiscione@gmail.com>
2016-10-07 16:32:00 -0600

    Beginning of OpeningBookTable / OpeningBookEntry

---
+++ src/components/OpeningBookEntry/OpeningBookEntry.js
@@ -0,0 +1,15 @@
+import React, { Component, PropTypes } from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './OpeningBookTable.css';
+
+//import {Table} from 'react-bootstrap';
+
+import {gameFromImmutable} from '../../store/model/gameState.js';
+
+function OpeningBookEntry({ clientID, gameID, gameplay, dispatchMoveCursor}) {
+  return(
+    <div>
+    </div>
+  );
+}
+
---
+++ src/components/OpeningBookTable/OpeningBookTable.js
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './OpeningBookTable.css';
+
+//import {Table} from 'react-bootstrap';
+
+import {gameFromImmutable} from '../../store/model/gameState.js';
+import OpeningBookEntry from '../OpeningBookEntry';
+
+function OpeningBookTable({ clientID, gameID, gameplay, dispatchMoveCursor}) {
+  return(
+    <div>
+    </div>
+  );
+}
+
