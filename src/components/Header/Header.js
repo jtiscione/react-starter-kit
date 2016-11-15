4a1bace05a613b404afcebe0b5b72a42dc14337b
Jason Tiscione <tiscione@gmail.com>
2016-11-14 18:37:52 -0700

    Fixed typo in Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -22,19 +22,19 @@ class Header extends React.Component {
   render() {
     return (
       /*
-      <div className={s.root}>
-        <div className={s.container}>
-          <Navigation className={s.nav} />
-          <Link className={s.brand} to="/">
-            <img src={logoUrl} width="38" height="38" alt="React" />
-            <span className={s.brandTxt}>Your Company</span>
-          </Link>
-          <div className={s.banner}>
-            <h1 className={s.bannerTitle}>React</h1>
-            <p className={s.bannerDesc}>Complex web apps made easy</p>
-          </div>
-        </div>
-      </div>
+       <div className={s.root}>
+       <div className={s.container}>
+       <Navigation className={s.nav} />
+       <Link className={s.brand} to="/">
+       <img src={logoUrl} width="38" height="38" alt="React" />
+       <span className={s.brandTxt}>Your Company</span>
+       </Link>
+       <div className={s.banner}>
+       <h1 className={s.bannerTitle}>React</h1>
+       <p className={s.bannerDesc}>Complex web apps made easy</p>
+       </div>
+       </div>
+       </div>
        Replaced the above with:
        */
       <Navbar>
@@ -53,5 +53,6 @@ class Header extends React.Component {
       </Navbar>
     );
   }
+}
 
 export default withStyles(s)(Header);
